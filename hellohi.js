7ddd6cd95c856889ded24fe52c9d3d41eb1d31cf
hoon43121 <rlacogbs123@gmail.com>
2023-08-11 20:58:21 +0900

    todo

---
+++ hellohi.js
@@ -0,0 +1,29 @@
+const toDoForm = document.querySelector("toDo-form");
+const toDoImput = document.querySelector("toDo-form input");
+const toDoList = document.querySelector("toDo-list");
+
+function deleteToDo() {
+    console.log('delete function');
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const button = document.createElement('button');
+    span.innerText = 'X';
+    button.addEventListener('click', deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+
+
+function handleTodoSubmit(event) {
+    event.preventDefault();
+    const newtodo = toDoinput.value;
+    toDoInput.value ='';
+    paintToDo(newTodo);
+}
+
+toDoForm.addEventListener('submit',handleTodoSubmit);
